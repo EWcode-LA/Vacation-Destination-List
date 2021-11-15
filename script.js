287291dd2e48721b9dbbd4d436f5c16783426654
EWcode-LA <seth.miller87@proton.mail>
2021-11-15 10:41:17 -0800

    scripts

---
+++ script.js
@@ -0,0 +1,8 @@
+var destination = document.querySelector("#destination_details_forms");
+//add event listener
+detailsForm.addEventListener("submit" handleFormSubmit);
+
+function handleFormSubmit(event){
+    event.preventDefault();
+    
+}
